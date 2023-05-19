909fe701de2e063fb64fadc7689f9f70f1621234
Michael Ernst <mernst@cs.washington.edu>
2023-05-19 13:21:35 -0700

    Elide irrelevant details

--- src/main/java/org/plumelib/util/package-info.java
+++ src/main/java/org/plumelib/util/package-info.java
@@ -11,10 +11,10 @@
  *       org.plumelib.util.WeakHasherMap WeakHasherMap}) that specifies the {@code hashCode()} and
  *       {@code equals()} methods.
  *   <dt>{@link org.plumelib.util.WeakHasherMap WeakHasherMap}
- *   <dd>WeakHashMap is a modified version of WeakHashMap from JDK 1.2.2, that adds a constructor
- *       that takes a {@link org.plumelib.util.Hasher Hasher} argument.
+ *   <dd>WeakHashMap is a modified version of WeakHashMap, that adds a constructor that takes a
+ *       {@link org.plumelib.util.Hasher Hasher} argument.
  *   <dt>{@link org.plumelib.util.WeakIdentityHashMap WeakIdentityHashMap}
- *   <dd>WeakIdentityHashMap is a modified version of WeakHashMap from JDK 1.5, that uses
+ *   <dd>WeakIdentityHashMap is a modified version of WeakHashMap, that uses
  *       System.identityHashCode() rather than the object's hash code.
  * </dl>
  */
